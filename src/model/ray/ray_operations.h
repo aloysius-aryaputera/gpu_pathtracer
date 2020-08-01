e3f2876943b33bbf5c6fcefb513daf59fb2977c9
aloysius-aryaputera <aloysius.aryaputera@gmail.com>
2020-08-01 15:53:47 +0800

    Refactor

--- src/model/ray/ray_operations.h
+++ src/model/ray/ray_operations.h
@@ -11,37 +11,24 @@
 
 __device__ Ray generate_ray(
   vec3 init_point, vec3 main_dir, vec3 normal, int mode, float n,
-	curandState *rand_state
+  curandState *rand_state
 );
 
 __device__ Ray generate_ray(
   vec3 init_point, vec3 main_dir, vec3 normal, int mode, float n,
-	curandState *rand_state
+  curandState *rand_state
 ) {
   CartesianSystem new_xyz_system;
   vec3 v3_rand, dir, v3_rand_world;
   if (mode == 0) {
-		new_xyz_system = CartesianSystem(normal);
+    new_xyz_system = CartesianSystem(normal);
     v3_rand = get_random_unit_vector_hemisphere_cos_pdf(rand_state);
-		v3_rand_world = new_xyz_system.to_world_system(v3_rand);
-		dir = unit_vector(v3_rand_world);
+    v3_rand_world = new_xyz_system.to_world_system(v3_rand);
+    dir = unit_vector(v3_rand_world);
   } else {
-
-    //if (main_dir.vector_is_nan()) {
-		//  printf("main_dir is nan\n");
-		//}
-
-		new_xyz_system = CartesianSystem(main_dir);
+    new_xyz_system = CartesianSystem(main_dir);
     v3_rand = get_random_unit_vector_phong(n, rand_state);
-		v3_rand_world = new_xyz_system.to_world_system(v3_rand);
-		//dir = unit_vector(v3_rand_world);
-
-		//if (
-		//	(dot(main_dir, normal) >= 0 && dot(dir, normal) <= 0) ||
-		//	(dot(main_dir, normal) <= 0 && dot(dir, normal) >= 0)
-		//) {
-		//	dir = main_dir;
-		//}
+    v3_rand_world = new_xyz_system.to_world_system(v3_rand);
   }
 
   return Ray(init_point, v3_rand_world);
